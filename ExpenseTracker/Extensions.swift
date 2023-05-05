aa3bf83a6cf5dbea6aaba5964f724afabb1b0013
Kevin Phan <ken.phan@live.ca>
2023-05-05 20:19:32 +0700

    navigation bell

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by phantom on 2023-05-05.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color("uiColor: .systemBackground")
+    
+    
+}
