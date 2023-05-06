1aa38bb0163ba9942188d3d597bc2b4ad9e33327
Kevin Phan <ken.phan@live.ca>
2023-05-06 23:58:00 +0700

    Add Preview data, Transaction Model and Row

---
+++ ExpenseTracker/TransactionModel.swift
@@ -0,0 +1,30 @@
+//
+//  TransactionModel.swift
+//  ExpenseTracker
+//
+//  Created by phantom on 2023-05-06.
+//
+
+import Foundation
+
+
+struct Transaction: Identifiable {
+    let id: Int
+    let date: String
+    let institution: String
+    let account: String
+    var merchant: String
+    let amount: Double
+    let type: TransactionType.RawValue
+    var categoryId: Int
+    var category: String
+    let isPending: Bool
+    var isTransfer: Bool
+    var isExpense: Bool
+    var isEdited: Bool
+}
+
+enum TransactionType: String {
+    case debit = "debit"
+    case credit = "credit"
+}
